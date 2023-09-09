a77f46655786464132d84a5791a5de21c36435fc
Yorch82 <jcampo1982@outlook.com>
2023-09-09 12:33:44 +0200

    first commit

---
+++ app/layout.js
@@ -0,0 +1,16 @@
+import './globals.css'
+import { Navigation } from './components/Navigation'
+
+export default function RootLayout ({ children }) {
+  return (
+    <html lang='en'>
+      <head>
+        <title>My first app with Next JS</title>
+      </head>
+      <body>
+        <Navigation />
+        {children}
+      </body>
+    </html>
+  )
+}
